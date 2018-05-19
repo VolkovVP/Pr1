3c4d24c87cfa9bd2127b291806a5815ddd0e5c56
VolkovVP <36610562+VolkovVP@users.noreply.github.com>
2018-05-19 17:58:17 +0300

    Update kvadratnoeur1.py

--- kvadratnoeur1.py
+++ kvadratnoeur1.py
@@ -1,5 +1,5 @@
 import math
-print("Введите коэффиценты a,b и c")
+print("Р’РІРµРґРёС‚Рµ РєРѕСЌС„С„РёС†РµРЅС‚С‹ a,b Рё c")
 a = float(input("a = "))
 b = float(input("b = "))
 c = float(input("c = "))
@@ -7,10 +7,10 @@ dis = b ** 2 - 4 * a * c
 print (a,"*x^2+",b,"+",c)
 d=b*b-4*a*c
 if (d < 0):
-    print("нет решений")
+    print("РЅРµС‚ СЂРµС€РµРЅРёР№")
 else:
     x1 = (-b + math.sqrt(d)) / (2 * a)
     x2 = (-b - math.sqrt(d)) / (2 * a)
     print("x1=",x1)
     print("x2=",x2)
-print("конец")
+print("РєРѕРЅРµС†")
